ecd04709069283a6d368ea9ecb7e8959bebebbb9
Jo1cindi <joancindi535@gmail.com>
2022-09-26 14:11:12 +0300

    routing

--- api/employees/employee.router.js
+++ api/employees/employee.router.js
@@ -3,16 +3,7 @@ const router = require("express").Router();
 const addEmployee = require("../../controllers/employee.controller")
 
 
-router.post("/", addEmployee, (req, res)=>{
-  try{
-    res.json({
-      status: 200,
-      message: "success"
-    })
-  }catch(error){
-    console.log(error)
-    res.status(500).send("server error")
-  }
-});
+router.post("/", addEmployee
+);
 
 module.exports = router;
