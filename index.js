02d7f90138288270ecebada25fa269eb2916b771
Jo1cindi <joancindi535@gmail.com>
2022-09-26 20:54:08 +0300

    editing index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/api/accountant", accountantRouter)
 const fmRouter  = require("./api/financemanager/financemanager.router")
 app.use("/api/finance-manager", fmRouter)
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT, ()=>{
     console.log(`Server is perfectly running on ${process.env.APP_PORT}`)
 })
 module.exports = app;
