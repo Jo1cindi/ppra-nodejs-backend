1be511ac9b798128a51f4a72080a0cbdfa3de66e
Jo1cindi <joancindi535@gmail.com>
2022-09-28 12:47:10 +0300

    adding cors

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser  = require("body-parser");
 const app = express();
-// const cors = require("cors")
+const cors = require("cors")
 
 app.use(express.json({extended: false}))
+app.use(cors())
 
 app.use(bodyParser.json());;
 
